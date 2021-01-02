4730517569d15ca8db73d45b1a6af91013865ed6
Subham Yadav <subhamy142@gmail.com>
2021-01-02 23:25:11 +0530

    added UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/posts';
+import { PostsList } from './';
 
 class App extends React.Component {
  componentDidMount(){
    this.props.dispatch(fetchPosts());
  }
 
- render(){
-   return <div>APP</div>
- }
+ render() {
+  const { posts } = this.props;
+  return (
+    <div>
+      <PostsList posts={posts} />
+    </div>
+  );
+}
 }
 
 function mapStateToProps(state){
   return{
-    posts : this.state.posts,
+    posts : state.posts,
   };
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
