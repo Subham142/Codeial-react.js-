95e089d82bed1967618244d4872c8636cba97cc9
Subham Yadav <subhamy142@gmail.com>
2021-01-04 21:25:27 +0530

    added action for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,59 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            email :'',
+            password : '',
+        };
+    }
+    
+    handleEmailChange = (e) => {
+        this.setState({
+            email: e.target.value,
+        })
+    }
+
+    handlePasswordChange = (e) => {
+        this.setState({
+            password: e.target.value,
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+         };
+
     render() {
         return (
             <form className="login-form">
-            <span className="login-signup-header">Log In</span>
-            <div className="field">
-              <input type="email" placeholder="Email" required />
-            </div>
-            <div className="field">
-              <input type="password" placeholder="Password" required />
-            </div>
-            <div className="field">
-              <button>Log In</button>
-            </div>
-          </form>
+        <span className="login-signup-header">Log In</span>
+        <div className="field">
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            // ref={this.emailInputRef}
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+          />
+        </div>
+        <div className="field">
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            // ref={this.passwordInputRef}
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
+          />
+        </div>
+        <div className="field">
+          <button onClick={this.handleFormSubmit}>Log In</button>
+        </div>
+      </form>
         )
     }
 }
