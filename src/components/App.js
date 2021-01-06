6e54f63a009fc4c42f182c1abb03961324216228
Subham Yadav <subhamy142@gmail.com>
2021-01-06 10:20:43 +0530

    solved the redirecting issue

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,11 @@ import {
 import PropTypes from 'prop-types';
 
 import { fetchPosts } from '../actions/posts';
-import { Home, Navbar, Page404, Login, Signup } from './';
+import { Home, Navbar, Page404, Login, Signup, Settings } from './';
 import jwtDecode from "jwt-decode";
 import { authenticateUser } from '../actions/auth';
 
-const Settings = () => <div>Setting</div>;
+//const Settings = () => <div>Setting</div>;
 
 const PrivateRoute = (privateRouteProps) => {
   const { isLoggedin, path, component: Component } = privateRouteProps;
@@ -22,7 +22,18 @@ const PrivateRoute = (privateRouteProps) => {
     <Route
       path={path}
       render={(props) => {
-        return isLoggedin ? <Component {...props} /> : <Redirect to="/login" />;
+        return isLoggedin ? (
+        <Component {...props} /> 
+        ):(
+           <Redirect 
+           to={{
+             pathname:'./login',
+             state:{
+               from : props.location,
+             },
+           }}
+        />
+      )
       }}
     />
   );
